4237d3f0c68f8574c3d9810ef4b862a760068cbc
KshitijRaj09 <59702912+KshitijRaj09@users.noreply.github.com>
2021-01-20 21:43:08 +0530

    Update maximum_minimum character.java

--- maximum_minimum character.java
+++ maximum_minimum character.java
@@ -1,3 +1,12 @@
+/*Create Solution class with main method.
+Read a String value using Scanner.
+The program should print largest and smallest character (as per ASCII sqeuence) together. 
+Consider below sample input and output (Output refers 'o' as largest and 'P' as smallest as we are following ASCII sequence):
+Input:
+Phone
+Output:
+oP*/
+
 import java.util.*;
 public class MyClass {
     public static void main(String args[]) {
