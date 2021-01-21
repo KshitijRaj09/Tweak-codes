482abb073aa6461910b3e535528861c1301a086d
KshitijRaj09 <59702912+KshitijRaj09@users.noreply.github.com>
2021-01-21 22:40:19 +0530

    Update reverse_digits.java

--- reverse_digits.java
+++ reverse_digits.java
@@ -10,12 +10,19 @@ public class MyClass {
     public static void main(String args[]) {
      Scanner sc = new Scanner(System.in);
      int num = sc.nextInt();
-     int num1=0,rem=0;
+     int num1=0,rem=0, max=Integer.MIN_VALUE, sum=0;
      while(num!=0){
         rem = num%10;
         num/=10;
+        if(max<rem){
+            max=rem;
+        }
+        sum+=rem;
         num1 = (num1*10)+rem; 
      }
-     System.out.print(num1);
+     System.out.println(num1);
+     System.out.println(sum);
+     System.out.println(max);
+     
     }
 }
